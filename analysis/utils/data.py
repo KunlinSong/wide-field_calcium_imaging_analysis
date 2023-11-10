8edf9dcbf07e42375959c33b32027ab23d9c865a
KunlinSong <kunlinsongcode@gmail.com>
2023-11-10 22:06:17 +0800

    update

--- analysis/utils/data.py
+++ analysis/utils/data.py
@@ -1,12 +1,20 @@
+import datetime
 import os
+from tkinter import filedialog
 from typing import *
 
 import numpy as np
+import pandas as pd
+import scipy.io as sio
 from skvideo.io import FFmpegReader
 from tifffile import imread
 
 
-class FramesMixin:
+def get_data_file() -> str:
+    return filedialog.askdirectory(title='Select a data file')
+
+
+class FramesInfo:
     def __init__(self, path: str) -> None:
         self.path = path
         self.filename = os.path.basename(path)
@@ -56,7 +64,7 @@ class FramesMixin:
         return self.width / self.height
 
 
-class Frames(FramesMixin):
+class Frames(FramesInfo):
     def __init__(self, path: str) -> None:
         try:
             super().__init__(path)
@@ -141,3 +149,80 @@ class CorrectedFrames:
         """
         os.makedirs(dirname, exist_ok=True)
         np.save(os.path.join(dirname, self.FILENAME), self._data)
+
+
+class FrameTimes:
+    KEY = 'frameTimes'
+
+    def __init__(self, path: str) -> None:
+        frame_times = sio.loadmat(path)[self.KEY].squeeze().tolist()
+        self.frame_times = [self._trans_time(time) for time in frame_times]
+    
+    @staticmethod
+    def _trans_time(matlab_time: float) -> datetime:
+        return (datetime.datetime.fromordinal(int(matlab_time)) 
+                + datetime.timedelta(days=matlab_time % 1) 
+                - datetime.timedelta(days = 366))
+
+
+class UltraSoundStimLog:
+    COLS = ['Begin', 'Params', 'End']
+    def __init__(self, path: str) -> None:
+        self.path = path
+        self.stim_data = self._get_stim_data()
+
+    def _get_stim_data(self) -> list[list[datetime.datetime, str, datetime.datetime]]:
+        with open(self.path, 'r') as f:
+            cont = f.readline()
+        cont = cont.strip('[]\n').split(',')
+        first_cont = cont[0].strip("'")
+        sign = first_cont[-6]
+        cont = [elem.strip("'") for elem in cont]
+        df_cont = []
+        for i, elem in enumerate(cont):
+            if i % 3 == 1:
+                df_cont.append(elem)
+            else:
+                elem_preprocessed = elem.rsplit(sign, 1)[0]
+                if i % 3 == 0:
+                    elem_preprocessed = elem_preprocessed.lstrip('begin:')
+                elif i % 3 == 2:
+                    elem_preprocessed = elem_preprocessed.lstrip('end:')
+                df_cont.append(datetime.datetime.strptime(elem_preprocessed, '%Y-%m-%d %H:%M:%S.%f'))
+        stim_len = len(self.COLS)
+        stims = [cont[i:i+stim_len] for i in range(0, len(cont), stim_len)]
+        return stims
+    
+    # TODO
+    def get_stim_lst(self, frame_times: FrameTimes) -> list[list[int, int]]:
+        pass
+
+
+class StimLog:
+    FILENAME = "stim_log.npy"
+    def __init__(self, stim_lst: list[list[int, int]]) -> None:
+        self.init_stim_log = stim_log.copy()
+        self.stim_log = stim_log.copy()
+    
+    def append(self, start: int, end: int) -> None:
+        self.stim_log.append([start, end])
+        self.stim_log.sort()
+    
+    def remove(self, idx: int) -> None:
+        self.stim_log.pop(idx)
+    
+    def clear(self) -> None:
+        self.stim_log = self.init_stim_log.copy()
+    
+    def reset(self) -> None:
+        self.stim_log = self.init_stim_log.copy()
+    
+    def save(self, dirname: str) -> None:
+        os.makedirs(dirname, exist_ok=True)
+        stim_log = np.array(self.stim_log)
+        np.save(os.path.join(dirname, self.FILENAME), stim_log)
+    
+    @classmethod
+    def load_from_file(cls, path: str) -> "StimLog":
+        stim_log = np.load(path)
+        return cls(stim_log.tolist())
